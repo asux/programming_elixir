69d27404f5e929541518269a383d571184faebe8
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2016-10-07 19:21:46 +0300

    Better solution by prasanna joshi

--- chapter07/my_list.exs
+++ chapter07/my_list.exs
@@ -1,17 +1,5 @@
 defmodule MyList do
-  def span(from, to) when from > to do
-    _span([from], -1, to)
-  end
-
-  def span(from, to) do
-    _span([from], 1, to)
-  end
-
-  defp _span([to | tail], _step, to) do
-    Enum.reverse([to | tail])
-  end
-
-  defp _span([head | tail], step, to) do
-    _span([head + step | [head | tail]], step, to)
-  end
+  def span(from, from), do: [from]
+  def span(from, to) when from > to, do: [from | span(from-1, to)]
+  def span(from, to), do: [from | span(from+1, to)]
 end
